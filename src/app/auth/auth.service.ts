26a509e8feb09fe53fef24025a1ab87c00f9a258
Manish Sahu <manissh7352@gmail.com>
2021-11-10 22:32:04 +0530

    code refactored

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,24 +4,36 @@ import { Injectable } from '@angular/core';
 interface UsernameAvailableResponse {
   available: boolean;
 }
+
+interface SignupCredetials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  baseUrl = 'https://api.angular-email.com';
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
     return this.http.post<UsernameAvailableResponse>(
-      'https://api.angular-email.com/auth/username',
+      `${this.baseUrl}/auth/username`,
       {
         username: username,
       }
     );
   }
 
-  signUp(credentials: any) {
-    return this.http.post<any>(
-      'https://api.angular-email.com/auth/signup',
+  signUp(credentials: SignupCredetials) {
+    return this.http.post<SignupResponse>(
+      `${this.baseUrl}auth/signup`,
       credentials
     );
   }
